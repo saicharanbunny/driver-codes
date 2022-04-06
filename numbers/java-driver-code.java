992bb08a072256dfc954bc508b027d11a38f35fb
saicharanbunny <98965023+saicharanbunny@users.noreply.github.com>
2022-04-06 18:49:32 +0530

    Update java-driver-code.java

--- numbers/java-driver-code.java
+++ numbers/java-driver-code.java
@@ -1,28 +1,26 @@
-import java.io.*;
 import java.util.*;
-
-public class Main {
-  	// The main function reads the input in the required format.
-  	// Just complete the solve function below.
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter wr = new PrintWriter(System.out);
-        int T = Integer.parseInt(br.readLine().trim());
-        for(int t_i = 0; t_i < T; t_i++)
-        {
-            int n =  Integer.parseInt(br.readLine());
-            String out_ = solve(n);
-            System.out.println(out_);
-         }
-         wr.close();
-         br.close();
+class Vinay {
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+       int n= sc.nextInt();
+       boolean flag = checkNumber(n);
+       if(flag) {
+           System.out.println("Special");
+       }else {
+           System.out.println("Regular");
+       }
     }
-  
-    static String solve(int n){
-      	// Your code goes here
-        // n is the given input
-      
-        String result = "Special";
-        return result;
+    public static boolean checkNumber(int n) {
+        boolean flag= true;
+        for(int i=2;i<=9;i++){
+            if(n%i==0){
+                if(n==2 || n==7) {
+                    continue;
+                }else {
+                    flag = false;
+                }
+            }
+        }
+        return flag;
     }
 }
